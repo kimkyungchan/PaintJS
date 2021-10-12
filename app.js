129e620802133323f84d58a7704a27bcb7ff9fbf
김경찬 <rlarudcks0339@naver.com>
2021-10-12 12:46:07 +0900

    context 추가

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "black";
+ctx.lineWidth = "2.5";
+
 let painting = false;
+
+function startPainting() {
+  painting = true;
+}
+
+function stopPainting() {
+  painting = false;
+}
 
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  console.log(x, y);
+  if (!painting) {
+    ctx.beginPath(); //선의 시작점을 알림
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y); //선의 시작path, 마지막path 연결
+    ctx.stroke(); // strokeStyle로 path를 표시
+  }
 }
 
 function onMouseDown(event) {
   painting = true;
-}
-
-function onMouseUp(event) {
-  painting = false;
 }
 
 function onMouseLeave(event) {
@@ -21,7 +36,7 @@ function onMouseLeave(event) {
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
-  canvas.addEventListener("mouseleave", onMouseLeave);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
 }
